0524ce37f2764026be903ec6f83b0e7908e352ef
Scott Sperling <scsper@gmail.com>
2013-03-05 23:39:07 -0800

    added a board to scrabble

--- board.py
+++ board.py
@@ -3,15 +3,48 @@ from enum import Multiplier, BoardPositionState
 class Board:
 	board = []
 	def __init__(self):
-		self.board = [[0 for col in range(15)] for row in range(15)]
+		self.board = [
+						[5,0,0,2,0,0,0,5,0,0,0,2,0,0,5],
+						[0,3,0,0,0,4,0,0,0,4,0,0,0,3,0],
+						[0,0,3,0,0,0,2,0,2,0,0,0,3,0,0],
+						[2,0,0,3,0,0,0,2,0,0,0,3,0,0,2],
+						[0,0,0,0,3,0,0,0,0,0,3,0,0,0,0],
+						[0,4,0,0,0,4,0,0,0,4,0,0,0,4,0],
+						[0,0,2,0,0,0,2,0,2,0,0,0,2,0,0],
+						[5,0,0,2,0,0,0,3,0,0,0,2,0,0,5],
+						[0,0,2,0,0,0,2,0,2,0,0,0,2,0,0],
+						[0,4,0,0,0,4,0,0,0,4,0,0,0,4,0],
+						[0,0,0,0,3,0,0,0,0,0,3,0,0,0,0],
+						[2,0,0,3,0,0,0,2,0,0,0,3,0,0,0],
+						[0,0,3,0,0,0,2,0,2,0,0,0,3,0,0],
+						[0,3,0,0,0,4,0,0,0,4,0,0,0,3,0],
+						[5,0,0,2,0,0,0,5,0,0,0,2,0,0,5]
+					]
+		build_board(self.board)
 		print self.board
+
+
+	def build_board(board):
+		for row in range(15):
+			for col in range(15):
+				board[row][col] = BoardPosition(board[row][col])
 
 
 # Board helper class
 class BoardPosition:
-	def __init__(self):
-		multiplier = Multiplier.NONE
-		state = BoardPositionState.OPEN
+	def __init__(self, value):
+		self.state = BoardPositionState.OPEN	
+		if value == 0:
+			self.multiplier = Multiplier.NONE
+		elif value == 2:
+			self.multiplier = Multiplier.DOUBLE_LETTER
+		elif value == 3:
+			self.multiplier = Multiplier.DOUBLE_WORD
+		elif value == 4:
+			self.multiplier = Multiplier.TRIPLE_LETTER
+		elif value == 5: 
+			self.multiplier = Multiplier.TRIPLE_WORD
+
 
 	def __str__(self):
 		return "My multiplier is " + self.multiplier + " and my state is " + self.state
